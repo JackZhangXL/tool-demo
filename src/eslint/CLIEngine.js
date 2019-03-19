f932647774dc4a0e5e81ee2ef1ba5b14f5b00f4d
zhangxinlin <zhangxinlin@meituan.com>
2019-03-19 19:08:42 +0800

    eslint

--- src/eslint/CLIEngine.js
+++ src/eslint/CLIEngine.js
@@ -1,87 +1,45 @@
 const glob = require("glob-promise");
-const fse = require("fs-extra");
-const path = require("path");
 const CLIEngine = require("eslint").CLIEngine;
-
-const babelParser = require('@babel/parser');
-const babelTraverse = require('@babel/traverse');
-const babelTypes = require('@babel/types');
-const babelGenerate = require('@babel/generator');
-
-const logTag = 'es5-check';
-
-const removeComments = (code) => {
-    const ast = babelParser.parse(code);
-
-    babelTraverse.default(ast, {
-        enter(path) {
-            babelTypes.removeComments(path.node);
-        },
-    });
-
-    return babelGenerate.default(ast).code;
-};
 
 const cli = new CLIEngine({
     useEslintrc: false,
-    configFile: path.join(__dirname, 'es5-check.json')
-    // rules: {
-    //     parserOptions: {
-    //         ecmaVersion: 5
-    //     }
-    // }
+    parserOptions: {
+        ecmaVersion: 5
+    }
 });
-// console.log(cli.getRules())
-const readFiles = async() => {
-    const files = await glob('src/eslint/tmp/*.js', { nodir: true });
 
-    const fileContentJobs = files.map(file => {
-        return fse.readFile(file, { encoding: 'utf-8' });
-    });
-    const fileContents = await Promise.all(fileContentJobs);
-    console.log(fileContents);
-
+const checkFiles = async() => {
     const errorMessages = [];
-    fileContents.forEach((fileContent, index) => {
-        const filePath = files[index];
-        console.log(filePath);
-        let errorMsg;
-        const transformedContent = removeComments(fileContent);
-        console.log(transformedContent);
+    let errorMsg = '';
+    const files = await glob('src/tmp/*.js', { nodir: true });
 
-        try {
-            const result = cli.executeOnText(transformedContent, filePath).results[0];
-            console.log(result);
+    try {
+        const results = cli.executeOnFiles(files).results;
 
+        results.forEach(result => {
             if (result.errorCount > 0) {
-                errorMsg = `${filePath}:\n`;
-                errorMsg += result.messages.map((message) => {
-                    const {
-                        message: msg,
-                        line,
-                        column,
-                    } = message;
-
-                    return `${msg}, line: ${line}, column: ${column}`;
+                errorMsg += `${result.filePath}:\n`;
+                errorMsg += result.messages.map(msg => {
+                    const { message, line, column } = msg;
+                    return `${message}, line: ${line}, column: ${column}\n`;
                 }).join('\n');
             }
-        } catch (e) {
-            // errorMsg = error(`${filePath}:\n error: ${e.message}`, logTag);
-        }
+        });
+    } catch (e) {
+        errorMsg = `es-5-check error: ${e.message}`;
+        console.error(errorMsg);
+    }
 
-        if (errorMsg) {
-            errorMessages.push(errorMsg);
-        }
-    });
+    if (errorMsg !== '') {
+        errorMessages.push(errorMsg);
+    }
 
     if (errorMessages.length > 0) {
-        // error(`failed: \n ${errorMessages.join('\n')} \n`, logTag);
+        console.error(`es-5-check failed: \n${errorMessages.join('\n')} \n`);
         process.exit(1);
     } else {
-        info('ok', logTag);
+        console.info('es-5-check ok');
     }
 };
 
-readFiles();
-
-
+checkFiles();
--- src/eslint/Linter.js
+++ src/eslint/Linter.js
@@ -1,36 +1,29 @@
 const Linter = require("eslint").Linter;
 const linter = new Linter();
 
-console.log(linter.getRules());     // 所有eslint的规则
-console.log(Linter.version);
-
-// const messages1 = linter.verify("var foo;", {
-//     rules: {
-//         semi: 2
+const messages1 = linter.verify("var foo", {
+    rules: {
+        semi: 2     // 2 - 开启规则，级别为 error，触发时会导致程序退出
+    }
+});
+console.log(messages1);
+// [
+//     {
+//         ruleId: 'semi',
+//         severity: 2,
+//         message: 'Missing semicolon.',
+//         line: 1,
+//         column: 8,
+//         nodeType: 'VariableDeclaration',
+//         fix: { range: [Array], text: ';' }
 //     }
-// }, { filename: "foo.js" });
-// console.log(messages1);
+// ]
+const messages2 = linter.verifyAndFix("var foo", {
+    rules: {
+        semi: 2
+    }
+});
+console.log(messages2);     // { fixed: true, messages: [], output: 'var foo;' }
 
-
-// or using SourceCode
-// const SourceCode = require("eslint").SourceCode;
-//
-// const code = new SourceCode("var foo = bar;", ast);
-//
-// const messages2 = linter.verify(code, {
-//     rules: {
-//         semi: 2
-//     }
-// }, { filename: "foo.js" });
-// console.log(messages2);
-
-
-// const messages3 = linter.verifyAndFix("var foo", {
-//     rules: {
-//         semi: 2
-//     }
-// });
-// console.log(messages3);
-
-
-
+// console.log(linter.getRules());     // 所有eslint的规则
+// console.log(Linter.version);
